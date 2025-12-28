5bae28a6a94caabddc4e16fce92423c3a793a4df
Nikita <nikitapudelko194@gmail.com>
2025-12-28 15:09:57 +0300

    Add keyboards package init

---
+++ bot/keyboards/__init__.py
@@ -0,0 +1,23 @@
+from .main import (
+    get_main_menu,
+    get_gender_keyboard,
+    get_interests_keyboard,
+    get_search_category_keyboard,
+    get_chat_menu,
+    get_report_reasons_keyboard,
+    get_premium_keyboard,
+    get_confirm_keyboard,
+    get_back_button
+)
+
+__all__ = [
+    'get_main_menu',
+    'get_gender_keyboard',
+    'get_interests_keyboard',
+    'get_search_category_keyboard',
+    'get_chat_menu',
+    'get_report_reasons_keyboard',
+    'get_premium_keyboard',
+    'get_confirm_keyboard',
+    'get_back_button'
+]
